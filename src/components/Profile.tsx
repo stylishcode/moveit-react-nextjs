bfefdbe27c832a2757a2d128cdb8ae45ebfb66e2
Matheus Henrique <matheus.hpp135@gmail.com>
2021-02-24 06:56:38 -0300

    Profile component created
    Next.js feature css module implemented

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+  return(
+    <div>
+      <img src="https://github.com/matheus3t.png" alt="Matheus Enrique"/>
+      <div>
+        <strong>Matheus Enrique</strong>
+        <p>Level 1</p>
+      </div>
+    </div>
+  );
+}
