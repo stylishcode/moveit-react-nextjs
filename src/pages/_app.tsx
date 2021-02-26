79f00695fcc56726706dfa64e1c2faf704a02c8b
Matheus Henrique <matheus.hpp135@gmail.com>
2021-02-26 13:46:47 -0300

    Refactor

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
-import '../styles/global.css';
-import { ChallengesProvider } from '../contexts/ChallengesContext';
- 
+import "../styles/global.css";
+
 function MyApp({ Component, pageProps }) {
   return (
     /* 
@@ -8,15 +7,18 @@ function MyApp({ Component, pageProps }) {
       Ou seja, como o app é algo que ficar por volta de toda a aplicação, quer dizer que toda a aplicação vai ter acesso
       aos dados do ChallengesContext
      */
-    <ChallengesProvider>
-      {/* A ordem de englobação depende de qual componente depende de qual, no caso Countdown depende de Challenges, 
+    /* 
+      <ChallengesProvider> index.tsx vai ser acessado por toda a aplicação também,
+      entãp, por necessidade do projeto, ele foi movido para lá
+     */
+
+    /* A ordem de englobação depende de qual componente depende de qual, no caso Countdown depende de Challenges, 
       então ele vai depois. Comentei o Countdown aqui porque não preciso que todos os componentes saibam dele, apenas o componente
-      que vai precisar, então englobei a section que precisa dele no index.tsx */}
-      {/* <CountdownProvider>  */}
-        <Component {...pageProps} />
-      {/* </CountdownProvider> */}
-    </ChallengesProvider>
-  )
+      que vai precisar, então englobei a section que precisa dele no index.tsx */
+    /* <CountdownProvider>  */
+
+    <Component {...pageProps} />
+  );
 }
 
-export default MyApp
+export default MyApp;
