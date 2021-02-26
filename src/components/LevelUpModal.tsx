fdd247ae36632b331fb3d971f7236f2ca748f8be
Matheus Henrique <matheus.hpp135@gmail.com>
2021-02-26 14:26:19 -0300

    LevelUpModal Finished

--- src/components/LevelUpModal.tsx
+++ src/components/LevelUpModal.tsx
@@ -1,15 +1,19 @@
+import { useContext, useState } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
 import styles from "../styles/components/LevelUpModal.module.css";
 
 export function LevelUpModal() {
+  const { level, closeLevelUpModal } = useContext(ChallengesContext);
+
   return (
     <div className={styles.overlay}>
       <div className={styles.container}>
-        <header>2</header>
+        <header>{level}</header>
 
         <strong>Parabéns</strong>
         <p>Você alcançou um novo level.</p>
 
-        <button type="button">
+        <button type="button" onClick={closeLevelUpModal}>
           <img src="/icons/close.svg" alt="Fechar modal" />
         </button>
       </div>
