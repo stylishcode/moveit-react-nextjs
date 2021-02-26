5b9667df02a81908b78fe8eb238b2a3220347b3f
Matheus Henrique <matheus.hpp135@gmail.com>
2021-02-26 07:03:27 -0300

    Two-component refactoring (context and component)

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,72 +1,38 @@
-import { useState, useEffect, useContext } from "react";
-import { ChallengesContext } from "../contexts/ChallengesContext";
+import { useContext } from "react";
+import { CountdownContext } from "../contexts/CountdownContext";
 import styles from "../styles/components/Countdown.module.css";
 
-/* variável de tipagem global para saber que é um tipo Timeout, é opcional */
-let countdownTimeout: NodeJS.Timeout;
-
 export function Countdown() {
-  const { startNewChallenge } = useContext(ChallengesContext);
+  const {
+    minutes,
+    seconds,
+    hasFinished,
+    isActive,
+    startCountdown,
+    resetCountdown,
+  } = useContext(CountdownContext);
 
-  const [time, setTime] = useState(0.1 * 60); /* 25 * 60 é 25min em segundos */
-  const [isActive, setIsActive] = useState(false); /* Verifica se o contador está parado ou em execução */
-  const [hasFinished, setHasFinished] = useState(false);
-
-  const minutes = Math.floor(time / 60);
-  const seconds = time % 60;
-  
   /* ConverterEmString.verificaSeTem2CharSenaoColocaUm0aEsquerda.SeparaOsCharEmArray */
-  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
-  const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
-
-  function startCountdown() {
-    /* impede que o setTimeOut execute mais uma vez mesmo depois de ser parado */
-    clearTimeout(countdownTimeout);
-    setIsActive(true);
-  }
-
-  function resetCountdown() {
-    setIsActive(false);
-    setTime(25 * 60); /* Volta ao valor inicial do useState */
-  }
-
-  /* useEffect é uma função para disparar efeitos colaterais, ou seja, quando algo mudar ou algo acontecer
-    eu quero executar alguma função, eu quero disparar algum efeito colateral
-    1º parâmetro: sempre uma função
-    2º parâmetro: quando que a função deve ser executada, que é passada dentro de um array, chamado array de
-    dependências do useEffect
-  */
-
-  useEffect(() => {
-    if (isActive && time > 0) { /* Se contador ativo e o tempo for maior que 0*/
-      countdownTimeout = setTimeout(() => { /* De 1 (1000ms) em 1 (1000ms) segundo, diminuir time em 1 */
-        setTime(time - 1);
-      }, 1000);
-    } else if (isActive && time === 0) {
-      setHasFinished(true); /* Finalizou o tempo */
-      setIsActive(false);
-      startNewChallenge();
-    }
-  }, [isActive, time]) /* isActive faz a função executar uma vez, time faz a função executar enquanto o if for verdade*/
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, "0").split("");
+  const [secondLeft, secondRight] = String(seconds).padStart(2, "0").split("");
 
   return (
     <div>
       <div className={styles.countdownContainer}>
         <div>
-          <span>{ minuteLeft }</span>
-          <span>{ minuteRight }</span>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
         </div>
 
         <span>:</span>
         <div>
-
-          <span>{ secondLeft }</span>
-          <span>{ secondRight }</span>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
         </div>
       </div>
 
       {/* Botão desabilitado que mostrar que o ciclo encerrou caso hasFinished for true */}
-      { hasFinished ? (
+      {hasFinished ? (
         <button
           disabled
           className={styles.countdownButton}
@@ -75,10 +41,12 @@ export function Countdown() {
           Ciclo encerrado
         </button>
       ) : (
-        <> {/*Elemento vazio, não aparece no html na página, serve para resolver o problema do react 
+        <>
+          {" "}
+          {/*Elemento vazio, não aparece no html na página, serve para resolver o problema do react 
             que pede para englobar essa parte do código*/}
           {/* Muda o botão dependendo do isActive */}
-          { isActive ? (
+          {isActive ? (
             <button
               type="button"
               className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
@@ -86,16 +54,15 @@ export function Countdown() {
             >
               Abandonar ciclo
             </button>
-
           ) : (
-              <button
-                type="button"
-                className={styles.countdownButton}
-                onClick={startCountdown}
-              >
-                Iniciar um ciclo
-              </button>
-            )}
+            <button
+              type="button"
+              className={styles.countdownButton}
+              onClick={startCountdown}
+            >
+              Iniciar um ciclo
+            </button>
+          )}
         </>
       )}
     </div>
---
+++ src/contexts/CountdownContext.tsx
@@ -0,0 +1,92 @@
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { ChallengesContext } from "./ChallengesContext";
+
+interface CountdownContextData {
+  minutes: number;
+  seconds: number;
+  isActive: boolean;
+  hasFinished: boolean;
+  startCountdown: () => void;
+  resetCountdown: () => void;
+}
+
+interface ChallengesProviderProps {
+  children: ReactNode;
+}
+
+export const CountdownContext = createContext({} as CountdownContextData);
+
+/* variável de tipagem global para saber que é um tipo Timeout, é opcional */
+let countdownTimeout: NodeJS.Timeout;
+
+export function CountdownProvider({ children }: ChallengesProviderProps) {
+  const { startNewChallenge } = useContext(ChallengesContext);
+
+  const [time, setTime] = useState(0.1 * 60); /* 25 * 60 é 25min em segundos */
+  /* Verifica se o contador está parado ou em execução */
+  const [isActive, setIsActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  function startCountdown() {
+    /* impede que o setTimeOut execute mais uma vez mesmo depois de ser parado */
+    clearTimeout(countdownTimeout);
+    setIsActive(true);
+  }
+
+  function resetCountdown() {
+    setIsActive(false);
+    setHasFinished(false);
+    setTime(0.1 * 60); /* Volta ao valor inicial do useState */
+  }
+
+  /* useEffect é uma função para disparar efeitos colaterais, ou seja, 
+    quando algo mudar ou algo acontecer, eu quero executar alguma função, 
+    eu quero disparar algum efeito colateral
+    1º parâmetro: sempre uma função
+    2º parâmetro: quando que a função deve ser executada, 
+    que é passada dentro de um array, chamado array de dependências do useEffect
+  */
+  useEffect(() => {
+    if (isActive && time > 0) {
+      /* Se contador ativo e o tempo for maior que 0*/
+      countdownTimeout = setTimeout(() => {
+        /* De 1 (1000ms) em 1 (1000ms) segundo, diminuir time em 1 */
+        setTime(time - 1);
+      }, 1000);
+    } else if (isActive && time === 0) {
+      setHasFinished(true); /* Finalizou o tempo */
+      setIsActive(false);
+      startNewChallenge();
+    }
+  }, [
+    isActive,
+    time,
+  ]); /* 
+        isActive faz a função executar uma vez, time faz a função executar, 
+        enquanto o if for verdade
+      */
+
+  return (
+    <CountdownContext.Provider
+      value={{
+        minutes,
+        seconds,
+        isActive,
+        hasFinished,
+        startCountdown,
+        resetCountdown,
+      }}
+    >
+      {children}
+    </CountdownContext.Provider>
+  );
+}
